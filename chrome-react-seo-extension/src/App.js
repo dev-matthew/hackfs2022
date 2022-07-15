2c55173864b225461bccf7b9035afba23bdcb409
Mach Solutions <m_fogel4@yahoo.com>
2022-07-15 00:07:00 -0700

    Setup XMTP functions

--- chrome-react-seo-extension/src/App.js
+++ chrome-react-seo-extension/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import {FiSettings} from 'react-icons/fi';
 import {IoMdClose} from 'react-icons/io';
 import {Wallet} from 'ethers';
+import {Client} from '@xmtp/xmtp-js';
 
-var wallet;
+var wallet, xmtp;
 
 export default function App() {
   const [activeLeft, setActiveLeft] = useState(0);
@@ -15,7 +16,7 @@ export default function App() {
   async function handlePrivateKey(privateKey, password) {
     try {
       wallet = new Wallet(privateKey);
-      if (password !== "") {
+      if (password) {
         let encrypted = await wallet.encrypt(password, setProgressWidth);
         localStorage.setItem("privateKey", encrypted);
       } else {
@@ -23,11 +24,13 @@ export default function App() {
       }
       console.log(wallet);
       reset();
+      await activateXMTP();
     } catch(e) {
       alert("Invalid private key");
     }
   }
 
+  // eslint-disable-next-line
   async function activateWallet(password) {
     if (password !== "") {
       try {
@@ -40,6 +43,31 @@ export default function App() {
     }
     console.log(wallet);
     reset();
+    await activateXMTP();
+  }
+
+  async function activateXMTP() {
+    xmtp = await Client.create(wallet);
+    await sendMessage("0xe2037FD7bEaF4E550C12719aDBdad50F39d3aAE5", "First Message", "uniswap.org")
+  }
+
+  async function sendMessage(address, message, website="None") {
+    let conversation = await xmtp.conversations.newConversation(address);
+    await conversation.send(website + "////" + message);
+    await getAllMessages(address);
+  }
+
+  async function getAllMessages(address) {
+    let conversation = await xmtp.conversations.newConversation(address);
+    let messages = await conversation.messages();
+    console.log(messages);
+  }
+
+  async function getAllConversations() {
+    let allConversations = await xmtp.conversations.list();
+    for (const conversation of allConversations) {
+      console.log(conversation.peerAddress);
+    }
   }
 
   function reset() {
@@ -49,16 +77,16 @@ export default function App() {
     setProgressWidth(0);
   }
 
-  useEffect(() => {
+  window.onload = function() {
     if (!passwordNeeded && localStorage.getItem("privateKey") !== null) {
       activateWallet("");
     }
-  }, [activateWallet, passwordNeeded])
+  }
 
   return (
     <div className="App">
       {settings && <div className="Settings">
-        {localStorage.getItem("privateKey") !== null && <IoMdClose className="ClosePrivateKey Icon"
+        {localStorage.getItem("privateKey") && <IoMdClose className="ClosePrivateKey Icon"
           onClick={() => {
             reset();
           }}/>}
@@ -92,21 +120,27 @@ export default function App() {
             onClick={() => setActiveLeft(0)}
             style={{
               color: activeLeft === 0 ? "white" : "rgba(255,255,255,0.5)"
-            }}>Sites</span>
+            }}>People</span>
           <span className="LeftNavHeaderOption"
             onClick={() => setActiveLeft(1)}
             style={{
               color: activeLeft === 1 ? "white" : "rgba(255,255,255,0.5)"
-            }}>People</span>
+            }}>Sites</span>
+          
           <FiSettings className="OpenSettings Icon"
             onClick={() => {
-              setActiveLeft(3);
+              setActiveLeft(2);
               setSettings(true);
             }}
             style={{
-              color: activeLeft === 3 ? "white" : "rgba(255,255,255,0.5)"
+              color: activeLeft === 2 ? "white" : "rgba(255,255,255,0.5)"
             }}/>
         </div>
+
+        <div className="NewMessage Option" onClick={() => {
+
+        }}>New Message</div>
+        <div className="Option Site">Words</div>
       </div>
       <div className="MiddleNav"></div>
     </div>
